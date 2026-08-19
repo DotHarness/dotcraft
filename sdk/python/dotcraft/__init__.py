e6f9e2b2c783f399838fde0ff9990ec293026d49
AkiKurisu <2683987717@qq.com>
2026-08-19 21:06:05 +0800

    bump version to v0.5.8

--- sdk/python/dotcraft/__init__.py
+++ sdk/python/dotcraft/__init__.py
@@ -41,7 +41,7 @@ from .models import (
 try:
     __version__ = version("dotcraft")
 except PackageNotFoundError:
-    __version__ = "0.5.7"
+    __version__ = "0.5.8"
 
 __all__ = [
     "DotCraft",
